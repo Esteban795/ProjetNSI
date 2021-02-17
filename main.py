dc2dea8edba778ea73ff1ee8596177c37f57dc49
esteban <estebanmandjukowski@gmail.com>
2021-02-17 22:03:45 +0100

    Removed unused code

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from tinydb import TinyDB
 
 #db = TinyDB("db.json")
 
-"""joueurs = int(input("Nombre de joueurs dans ce tournoi : "))
+joueurs = int(input("Nombre de joueurs dans ce tournoi : "))
 nom_tournoi = input("Nom du tournoi : ")
 lieu_tournoi = input("Lieu du tournoi : ")
 date_tournoi = input("Date du tournoi : ")
 tours = int("".join([str(int(joueurs/2)) if joueurs%2 == 0 else str(int(joueurs/2) + 1)]))
-tournoi = Tournoi(joueurs,nom_tournoi,lieu_tournoi,date_tournoi,tours)"""
+tournoi = Tournoi(joueurs,nom_tournoi,lieu_tournoi,date_tournoi,tours)
 
-tournoi = Tournoi(4,"Tournoi du Mans","Mans","11/08/2003",2)
 tournoi.ajouter_joueurs()
 tournoi.afficher_joueurs()
 tournoi.premier_tour()
