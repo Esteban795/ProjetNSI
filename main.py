839303cff0e5d54e9689f2d36c37279a5725d027
Esteban <estebanmandjukowski@gmail.com>
2021-03-02 19:07:13 +0100

    Restarting everything on main

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from tournoi import Tournoi
 from player import Player
-from tinydb import TinyDB
-
-#db = TinyDB("db.json")
-
-joueurs = int(input("Nombre de joueurs dans ce tournoi : "))
-nom_tournoi = input("Nom du tournoi : ")
-lieu_tournoi = input("Lieu du tournoi : ")
-date_tournoi = input("Date du tournoi : ")
-tours = int("".join([str(int(joueurs/2)) if joueurs%2 == 0 else str(int(joueurs/2) + 1)]))
-tournoi = Tournoi(joueurs,nom_tournoi,lieu_tournoi,date_tournoi,tours)
-
-tournoi.ajouter_joueurs()
-tournoi.afficher_joueurs()
-tournoi.premier_tour()
-tournoi.reste_tournoi()
+from match import Match,Round
+from player import Player
+import json
