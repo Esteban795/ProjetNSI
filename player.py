3bf3edda8f9b85b6c53d66ed0b3ce260a273cdfe
Esteban <estebanmandjukowski@gmail.com>
2021-03-03 18:23:30 +0100

    Added an identifier to help with the GUI

--- player.py
+++ player.py
@@ -1,10 +1,11 @@
 class Player:
-    def __init__(self,nom_famille,prenom,date_naissance,sexe,classement:int):
+    def __init__(self,nom_famille,prenom,date_naissance,sexe,classement:int,identifier:int):
         self.nom_famille = nom_famille
         self.prenom = prenom
         self.date_naissance = date_naissance
         self.sexe = sexe
         self.classement = classement
+        self.id = identifier
 
     def serialize(self):
         return self.__dict__
